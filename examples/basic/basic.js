c293ebaa0596be9eb543ecea456789e509b1fa2f
Gajus Kuizinas <gk@anuary.com>
2014-10-25 18:40:25 +0300

    Clean up.

--- examples/basic/basic.js
+++ examples/basic/basic.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     targetElement = document.querySelector('.target');
     pan = new Pan(targetElement);
 
-    console.log('cssTransform', cssTransform);
-
     pan.eventEmitter.on('move', function (e) {
         e.handle.style[cssTransform] = 'translate(' + e.offsetX + 'px,' + e.offsetY + 'px)';
     });
