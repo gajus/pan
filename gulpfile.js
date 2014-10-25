19698af7c31bfa57e2f13f66baa7f6550d1a23ef
Gajus Kuizinas <gk@anuary.com>
2014-10-25 13:10:45 +0300

    Min version.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 var pkg = require('./package.json'),
     gulp = require('gulp'),
     jshint = require('gulp-jshint'),
+    header = require('gulp-header'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
     browserify = require('gulp-browserify'),
+    fs = require('fs'),
     del = require('del');
 
 gulp.task('lint', function () {
@@ -24,8 +28,29 @@ gulp.task('bundle', ['clean'], function () {
         .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('version', ['bundle'], function () {
+    var bower = require('./bower.json');
+
+    gulp
+        .src('./dist/pan.js')
+        .pipe(header('/**\n* @version <%= version %>\n* @link https://github.com/gajus/pan for the canonical source repository\n* @license https://github.com/gajus/pan/blob/master/LICENSE BSD 3-Clause\n*/\n', {version: pkg.version}))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(uglify())
+        .pipe(rename('pan.min.js'))
+        .pipe(header('/**\n* @version <%= version %>\n* @link https://github.com/gajus/pan for the canonical source repository\n* @license https://github.com/gajus/pan/blob/master/LICENSE BSD 3-Clause\n*/\n', {version: pkg.version}))
+        .pipe(gulp.dest('./dist/'));
+
+    bower.name = pkg.name;
+    bower.description = pkg.description;
+    bower.version = pkg.version;
+    bower.keywords = pkg.keywords;
+    bower.license = pkg.license;
+
+    fs.writeFile('./bower.json', JSON.stringify(bower, null, 4));
+});
+
 gulp.task('watch', function () {
     gulp.watch(['./src/*', './package.json'], ['default']);
 });
 
-gulp.task('default', ['bundle']);
+gulp.task('default', ['version']);
